eaab1277523ae9ff9ab925b2d5b1a84e56bfacf2
DanielWojciechowski <daniel.wojciechowski.91@gmail.com>
2015-06-10 00:43:28 +0200

    -servis udostępniający wycieczki wybranego klienta -callback (JSONP)

--- src/main/java/com/github/danielwojciechowski/TTServer/mongoServer/ServerStarter.java
+++ src/main/java/com/github/danielwojciechowski/TTServer/mongoServer/ServerStarter.java
@@ -2,10 +2,7 @@ package com.github.danielwojciechowski.TTServer.mongoServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
@@ -13,17 +10,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories("com.github.danielwojciechowski.TTServer.repositories")
 public class ServerStarter {
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
-        MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
-
-        //db init
-/*        Travel travel = new Travel();
-        travel.setTrace(new LinkedList<GeoPoint>());
-        travel.getTrace().add(new GeoPoint(52.251605, 20.998186, new Date()));
-        travel.getTrace().add(new GeoPoint(52.251202, 20.998266, new Date()));
-        travel.getTrace().add(new GeoPoint(52.250961, 20.998322, new Date()));
-        mongoOperation.save(travel);*/
-
         SpringApplication.run(ServerStarter.class, args);
     }
 }
--- src/main/java/com/github/danielwojciechowski/TTServer/repositories/TravelRepo.java
+++ src/main/java/com/github/danielwojciechowski/TTServer/repositories/TravelRepo.java
@@ -2,25 +2,12 @@ package com.github.danielwojciechowski.TTServer.repositories;
 
 import com.github.danielwojciechowski.TTServer.datamodel.Travel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface TravelRepo extends MongoRepository<Travel, String> {
-
-/*    default void m(@Param("id") String id){
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
-        MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
-        Query query6 = new Query();
-        query6.addCriteria(Criteria.where("id").is("appleF"));
-
-        Update update6 = new Update();
-        update6.set("age", 101);
-        update6.set("ic", 1111);
-
-        //FindAndModifyOptions().returnNew(true) = newly updated document
-        //FindAndModifyOptions().returnNew(false) = old document (not update yet)
-        Travel userTest6 = mongoOperation.findAndModify(
-                query6, update6,
-                new FindAndModifyOptions().returnNew(true), Travel.class);
-    }*/
+        List<Travel> findTravelsByUserUID(@Param("uid") String uid);
 }
